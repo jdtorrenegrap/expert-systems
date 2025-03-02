8f24c1e3abecb689fb7d6a6c6458d33d6ae766f3
Jesus Torrenegra <jdtrorrenegrap@ul.edu.co>
2025-03-02 14:37:13 -0500

    is added: log of responses and displayed in the query.

--- src/models/query.js
+++ src/models/query.js
@@ -1,7 +1,8 @@
 import sql from '../config/db.js';
 
 // Registrar diagnóstico
-async function registerDiagnostic(username, dni, diagnosis) {
+async function registerDiagnostic(username, dni, diagnosis, responses) {
+
     try {
         const userExists = await sql`SELECT id FROM users WHERE dni = ${dni}`;
         let user_id;
@@ -9,37 +10,42 @@ async function registerDiagnostic(username, dni, diagnosis) {
         if (userExists.length === 0) {
             const register = await sql`INSERT INTO users (username, dni) 
                 VALUES (${username}, ${dni}) 
-                RETURNING id
-            `;
+                RETURNING id`;
             user_id = register[0].id;
+
         } else {
             user_id = userExists[0].id;
         }
 
-        await sql`
-            INSERT INTO diagnostics (user_id, diagnosis)
+        const diagnosticEntry = await sql` INSERT INTO diagnostics (user_id, diagnosis)
             VALUES (${user_id}, ${diagnosis})
-        `;
+            RETURNING id`;
 
-        return { message: 'Diagnóstico registrado con éxito' };
+        const diagnosis_id = diagnosticEntry[0].id;
+
+        await sql`
+            INSERT INTO responses (user_id, diagnosis_id, p1, p2, p3, p4, p5)
+            VALUES (${user_id}, ${diagnosis_id}, ${responses.p1}, ${responses.p2}, ${responses.p3}, ${responses.p4}, ${responses.p5})`;
+        return { message: 'Registro con éxito' };
+
     } catch (error) {
-        console.error('Error al registrar el diagnóstico:', error);
+        console.error('Error al registrar el diagnóstico y respuestas:', error);
         throw error;
     }
 }
+
 // Obtener diagnóstico
 async function getDiagnostic(dni) {
     try {
-        const user = await sql`SELECT * FROM get_diagnosis(${dni})`;
-
+        const user = await sql`SELECT * FROM get_diagnosis_two(${dni})`;
         if (user.length === 0) {
             return null;
         }
         return user;
+
     } catch (error) {
         console.error('Error al obtener los diagnósticos:', error);
         throw error;
     }
 }
-
 export { registerDiagnostic, getDiagnostic };
--- src/services/regDiagnosticController.js
+++ src/services/regDiagnosticController.js
@@ -14,20 +14,29 @@ export const handleDiagnostic = async (req, res) => {
             question_3,
             question_4,
             question_5
-        }
+        };
+        const response = {
+            p1: question_1,
+            p2: question_2,
+            p3: question_3,
+            p4: question_4,
+            p5: question_5
+        };
 
         const results = await runRules(symptoms)
         const diagnostics = results.events.map(event => event.params.diagnostico)
 
         // Save the diagnostic in the database
         for (const i of diagnostics) {
-            await registerDiagnostic(username, dni, i)
+            await registerDiagnostic(username, dni, i, response)
         }
         res.status(200).json({ message: 'Diagnóstico registrado con éxito', diagnostics })
+        console.log(`Diagnóstico registrado con éxito: ${diagnostics}`)
         
     } catch (error) {
         console.error('Error al procesar el diagnóstico:', error)
         res.status(500).json({ message: 'Error al procesar el diagnóstico' })
+       console.log(`Error al procesar el diagnóstico: ${error}`)
     }
 }
 
@@ -42,6 +51,7 @@ export const handleGetDiagnostic = async (req, res) => {
             return res.status(404).json({ message: 'No se encontraron diagnósticos con el DNI proporcionado' })
         }
         res.status(200).json({ diagnostics })
+        console.log(`Diagnósticos encontrados: ${diagnostics}`)
 
     } catch (error) {
         console.error('Error al obtener el diagnóstico:', error)
